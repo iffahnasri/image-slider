3688d737b16279599dab481efd527e31acf9bbe3
iffahnasri <73266611+iffahnasri@users.noreply.github.com>
2024-01-13 00:30:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-// script.js
-// JavaScript code for the image slider
-var currentSlide = 0;
-var slides = document.querySelectorAll(".slider img");
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let slideIndex = 0;
 
-function showSlide(index) {
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[index].style.display = "block";
+prevBtn.addEventListener('click', () => {
+    slideIndex = (slideIndex - 1 + slider.children.length) % slider.children.length;
+    updateSlider();
+});
+
+nextBtn.addEventListener('click', () => {
+    slideIndex = (slideIndex + 1) % slider.children.length;
+    updateSlider();
+});
+
+function updateSlider() {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
 }
-
-showSlide(currentSlide);
-
-document.querySelector(".next").addEventListener("click", function () {
-    currentSlide++;
-    if (currentSlide >= slides.length) {
-        currentSlide = 0;
-    }
-    showSlide(currentSlide);
-});
-
-document.querySelector(".prev").addEventListener("click", function () {
-    currentSlide--;
-    if (currentSlide < 0) {
-        currentSlide = slides.length - 1;
-    }
-    showSlide(currentSlide);
-});
